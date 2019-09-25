91d566c92fa25030d6f5e2d2a566d5257f2c422a
siim <siim@dolmit.com>
2019-09-25 21:27:15 +0300

    Add main statuses migration

--- src/Repository/StatusRepository.php
+++ src/Repository/StatusRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 class StatusRepository
 {
     public const TABLE_NAME = 'status';
+
     public const STATUS_ACTIVE = 'active';
     public const STATUS_DELETED = 'deleted';
 }
