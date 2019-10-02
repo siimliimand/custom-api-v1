8a6e7a041481b37c093f480b00b899ce478852ac
siim <siim@dolmit.com>
2019-10-02 21:40:03 +0300

    Fix SetterGetter class

--- src/RequestHandler/SetterGetter.php
+++ src/RequestHandler/SetterGetter.php
@@ -17,6 +17,7 @@ class SetterGetter
     public function __construct(string $type)
     {
         $this->type = $type;
+        $this->init();
     }
 
     /**
@@ -39,29 +40,6 @@ class SetterGetter
     {
         $key = strtolower($key);
         $value = $this->data[$key] ?? null;
-        if ($value === null) {
-            if ($this->type === static::TYPE_HEADERS) {
-                $fn = $this->type;
-                foreach ($fn() as $k => $v) {
-                    if (strtolower($k) === $key) {
-                        return $v;
-                    }
-                }
-                return null;
-            }
-            if ($this->type === static::TYPE_POST) {
-                $contents = json_decode(file_get_contents('php://input'), true);
-                if (is_array($contents) && array_key_exists($key, $contents)) {
-                    return $contents[$key];
-                }
-            }
-            foreach ($GLOBALS[$this->type] as $k => $v) {
-                if (strtolower($k) === $key) {
-                    return $v;
-                }
-            }
-            return $default;
-        }
 
         if ($value === null) {
             return $default;
@@ -70,32 +48,59 @@ class SetterGetter
         return $value;
     }
 
+    protected function init(): void
+    {
+        switch ($this->type) {
+            case static::TYPE_POST:
+                $this->setArray($_POST);
+                $contents = json_decode(file_get_contents('php://input'), true);
+                if (is_array($contents)) {
+                    $this->setArray($contents);
+                }
+                break;
+
+            case static::TYPE_HEADERS:
+                $fn = $this->type;
+                $data = $fn();
+                if (is_array($data)) {
+                    $this->setArray($data);
+                }
+                break;
+
+            case static::TYPE_FILES:
+                $this->setArray($_FILES);
+                break;
+
+            case static::TYPE_COOKIE:
+                $this->setArray($_COOKIE);
+                break;
+
+            case static::TYPE_GET:
+                $this->setArray($_GET);
+                break;
+
+            case static::TYPE_SERVER:
+                $this->setArray($_SERVER);
+                break;
+        }
+    }
+
+    /**
+     * @param array $vkeValuePairs
+     */
+    protected function setArray(array $vkeValuePairs): void
+    {
+        foreach ($vkeValuePairs as $key => $value) {
+            $this->set($key, $value);
+        }
+    }
+
     /**
      * @return array
      */
     public function all(): array
     {
-        if ($this->type === static::TYPE_HEADERS) {
-            $fn = $this->type;
-            $data = $fn();
-        } else {
-            $data = $GLOBALS[$this->type];
-        }
-        if ($this->type === static::TYPE_POST) {
-            $contents = json_decode(file_get_contents('php://input'), true);
-            if (is_array($contents)) {
-                foreach ($contents as $key => $value) {
-                    $data[$key] = $value;
-                }
-            }
-        }
-
-        $newData = [];
-        foreach($data as $key => $value) {
-            $newData[strtolower($key)] = $value;
-        }
-
-        return array_replace($newData, $this->data);
+        return $this->getData();
     }
 
     /**
